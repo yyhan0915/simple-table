26e189092b8becd563d21f475decd990dfe5e28c
yyhan0915 <yyhan2059@hanmail.net>
2020-12-19 15:40:17 +0100

    test config fixed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,15 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import Link from 'next/link';
+import React from 'react';
 
 const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
+    <>
+        <h1>Hello Next.js </h1>
+        <p>
+            <Link href="/about">
+                <a>About</a>
+            </Link>
+        </p>
+    </>
+);
 
-export default IndexPage
+export default IndexPage;
